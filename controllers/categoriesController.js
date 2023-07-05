03c62ca25055896e9059c71e564d51a6695c655f
MazenShebeta <mazenshebeta@gmail.com>
2023-07-05 22:34:13 +0300

    categories functionallity

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -15,6 +15,29 @@ class categories {
     }
   }
 
+  static async update(req, res) {
+    try {
+      const category = await Category.findById(req.params.id);
+      if (req.user.role != "admin") res.status(403).json("Not authorized!");
+      category.update(req.body);
+      await category.save();
+      res.status(200).json({ message: "Category has been updated", category });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+
+  static async delete(req, res) {
+    try {
+      const category = await Category.findById(req.params.id);
+      if (req.user.role != "admin") res.status(403).json("Not authorized!");
+      await category.delete();
+      res.status(200).json("Category has been deleted");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+
   // Get all categories
   static async getAll(req, res) {
     try {
--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -1,10 +1,14 @@
 const router = require("express").Router();
 const Category = require("../controllers/categoriesController");
-
-// Create new category
-router.post("/", Category.create);
+const auth = require("../middlewares/auth");
 
 // Get all categories
-router.get("/", Category.getAll);
+router.get("/", auth, Category.getAll);
+// Create new category
+router.post("/", auth, Category.create);
+// Update category
+router.put("/:id", auth, Category.update);
+// Delete category
+router.delete("/:id", auth, Category.delete);
 
 module.exports = router;
